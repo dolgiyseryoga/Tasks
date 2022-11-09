ad60ab85b1992d7640ab6137e14c8702e25e8520
unknown <you@example.com>
2022-11-09 16:11:13 +0300

    image+func

--- scripts.js
+++ scripts.js
@@ -1,43 +1,65 @@
-function tasks(){
+function tasks() {
 
-      let a = prompt('Введите число a', '');
-      let b = prompt('Введите число b', '');
+  let a = prompt('Введите число a', '');
+  let b = prompt('Введите число b', '');
 
-      function number(a,b){ 
-      if((!isNaN(parseFloat(a)) && isFinite(a)) && (!isNaN(parseFloat(b)) && isFinite(b))){
-      function min(a,b) {
-        if (a > b)  { alert('Меньшее = '+ b);}
-        if (a < b)  { alert('Меньшее = '+ a);}
-        if (a == b) { alert('Числа '+a+' и '+b+' равны!');}
+  function number(a, b) {
+    if ((!isNaN(parseFloat(a)) && isFinite(a)) && (!isNaN(parseFloat(b)) && isFinite(b))) {
+      function min(a, b) {
+        if (a > b) { alert('Меньшее = ' + b); }
+        if (a < b) { alert('Меньшее = ' + a); }
+        if (a == b) { alert('Числа ' + a + ' и ' + b + ' равны!'); }
       }
-      min(a,b);
-      }
-        else {
+      min(a, b);
+    }
+    else {
       alert('Это не числа!');
-      }
-      };
-      number(a,b);
+    }
+  };
+  number(a, b);
 }
 
-function tasks2(){
+function tasks2() {
 
   let x = prompt('Введите число x', '');
-let n = prompt('Введите число n', '');
-function pow(x,n) {
-  if((!isNaN(parseFloat(x)) && isFinite(x)) && (!isNaN(parseFloat(n)) && isFinite(n))){
-    return Math.pow(x, n);
-}
-  else {
-    alert('Это не числа!');
-    exit();}
-}
-alert(pow(x, n));
+  let n = prompt('Введите число n', '');
+  function pow(x, n) {
+    if ((!isNaN(parseFloat(x)) && isFinite(x)) && (!isNaN(parseFloat(n)) && isFinite(n))) {
+      return Math.pow(x, n);
+    }
+    else {
+      alert('Это не числа!');
+      exit();
+    }
+  }
+  alert(pow(x, n));
 
 }
+function tasks3() {
 
-function tasks3(){
 
-  alert("Вы нажали на кнопку3");
+  let salaries = {
+    John: 100,
+    Ann: 7,
+    Pete: 240
+  }
+
+  let salari = Object.assign(salaries, {      });
+
+ alert(salari.Ann);
+ 
 
 }
 
+
+
+
+function closeWindow(){
+  if (confirm("Нажмите 'OK' чтобы уйти со страницы, или 'Отмена' если передумали. "))
+  {         
+     window.location="http://google.com";
+  }
+    else{  
+      location.reload();
+  }
+}
